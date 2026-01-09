6451fa994960116ae9e194df6a00159e09daa89e
Vineet-bellary <vineet.bellary@gmail.com>
2026-01-10 02:23:41 +0530

    dataset code only adde

--- dataset_download.py
+++ dataset_download.py
@@ -16,9 +16,10 @@ print("Dataset downloaded successfully...")
 
 
 # This is to download Dataset 2 from Roboflow
-# rf = Roboflow(api_key=os.getenv("ROBOFLOW_API_KEY"))
-# project = rf.workspace("small-object-detections-smart-surveillance-system").project(
-#     "cgi-weapon-dataset-q6mia"
-# )
-# version = project.version(2)
-# dataset = version.download("tensorflow")
+rf = Roboflow(api_key=os.getenv("ROBOFLOW_API_KEY"))
+project = rf.workspace("small-object-detections-smart-surveillance-system").project(
+    "cgi-weapon-dataset-q6mia"
+)
+version = project.version(2)
+dataset = version.download("tensorflow")
+print("Dataset downloaded successfully...")
